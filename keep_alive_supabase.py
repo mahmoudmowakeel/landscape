cfc3bb5c987a0ee72a257a2afdb2532055ea6e10
ToqaBahaa <68813404+ToqaBahaa@users.noreply.github.com>
2025-04-06 00:25:07 +0200

    keep_alive_supabase.py

--- keep_alive_supabase.py
+++ keep_alive_supabase.py
@@ -1,11 +1,18 @@
 import requests
 
-url = "https://aaoihfjirymbeuwfgght.supabase.co"
+url = "https://aaoihfjirymbeuwfgght.supabase.co/auth/v1/admin/users"
 headers = {
-    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2loZmppcnltYmV1d2ZnZ2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NjA3ODksImV4cCI6MjA1ODMzNjc4OX0.qM5VR4tzWwg0X7Q0lJy9-MWX3KaXKU_hLalJ1mmcx0s",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2loZmppcnltYmV1d2ZnZ2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3NjA3ODksImV4cCI6MjA1ODMzNjc4OX0.qM5VR4tzWwg0X7Q0lJy9-MWX3KaXKU_hLalJ1mmcx0s",
+    "apikey": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2loZmppcnltYmV1d2ZnZ2h0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mjc2MDc4OSwiZXhwIjoyMDU4MzM2Nzg5fQ.EjY5dxVUU5UFE-hiwQl43pgSIIKWC7EckhKJ_I6k-8s',
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhb2loZmppcnltYmV1d2ZnZ2h0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mjc2MDc4OSwiZXhwIjoyMDU4MzM2Nzg5fQ.EjY5dxVUU5UFE-hiwQl43pgSIIKWC7EckhKJ_I6k-8s",
     "Content-Type": "application/json"
 }
 
 response = requests.get(url, headers=headers)
+
 print("Status:", response.status_code)
+
+try:
+    response_data = response.json()
+    print("Response:", response_data)
+except ValueError as e:
+    print("Failed to decode JSON:", e)
